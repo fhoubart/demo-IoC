e119217b5fe3eb5b9df9bc7c2f70e790ad0726ac
HOUBART FLORENT <florent.houbart@gmail.com>
2022-06-27 16:14:01 +0200

    controller

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,6 +1,17 @@
+import { User } from "../entities";
+import { LoginService } from "../services/LoginService";
+import { SessionService } from "../services/SessionService";
+
 export class LoginContoller {
 
-    public checkLogin(user:string, password: string):void {
-        
+    public async checkLogin(user:string, password: string):Promise<void> {
+        let loginService:LoginService = ????;
+        let sessionService:SessionService = ???;
+
+        let userObject:User = await loginService.login(user,password);
+        await sessionService.saveUser(userObject);
+
+        // Redirection de la page
+
     }
 }
